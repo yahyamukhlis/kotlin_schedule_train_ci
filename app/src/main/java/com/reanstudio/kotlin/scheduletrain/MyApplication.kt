3ae041c0729adf3ab119ff5edf8d415538ebe053
Yahya Muhammad Mukhlis <yahya.mukhlis@gmail.com>
2017-01-18 12:04:32 +0700

    add stetho configuration

--- app/src/main/java/com/reanstudio/kotlin/scheduletrain/MyApplication.kt
+++ app/src/main/java/com/reanstudio/kotlin/scheduletrain/MyApplication.kt
@@ -3,6 +3,7 @@ package com.reanstudio.kotlin.scheduletrain
 import android.content.Context
 import android.support.multidex.MultiDex
 import android.support.multidex.MultiDexApplication
+import com.facebook.stetho.Stetho
 
 /**
  * Created by yahyamukhlis on 1/17/17.
@@ -13,5 +14,17 @@ class MyApplication : MultiDexApplication() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeStetho()
+    }
+
+    private fun initializeStetho() {
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
     }
 }
